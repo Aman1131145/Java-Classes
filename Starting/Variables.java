d0de0a6ad8b099c01cc69ca8887b8d20d3642db1
Aman Mishra <mishraaman113114@gmail.com>
2022-10-13 14:34:02 +0530

    day 1

---
+++ Starting/Variables.java
@@ -0,0 +1,12 @@
+package Starting;
+
+public class Variables {
+
+    public static void main(String[] args){
+        //let a right angle triangle have length 4 and base 5
+        int length = 4;
+        int base = 5;
+        int sqhype = length*length+base*base;
+        System.out.println(sqhype);
+    }   
+}
---
+++ Starting/hello.java
@@ -0,0 +1,6 @@
+package Starting;
+public class hello {
+    public static void main (String [] args){
+        System.out.println("sonu");
+    }
+}
